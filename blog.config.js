82761fa43ceb9af215f5628d795317d4913f7eab
大青蛙 <50044804+BethelDEV@users.noreply.github.com>
2024-11-13 16:28:56 +0800

    Update blog.config.js
    taloseng blog description

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'Talos Engineering',
+  author: 'TalosEng',
+  email: 'taloseng@outlook.com',
+  link: 'https://www.taloseng.com',
+  description: 'A comprehensive blog for mechanical engineering, manufacturing trends, innovations, and industry insights.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
